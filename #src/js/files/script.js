cfeb1030cf95c50fbcd5806231fc251132ee5e65
maksmt <maksmtoff@gmail.com>
2022-01-18 21:18:30 +0200

    first commit

---
+++ #src/js/files/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('.market__button-slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        speed: 1200,
+    });
+});
+
+
+$(document).ready(function () {
+    $('.button__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1400,
+
+    });
+
+});
+$(document).ready(function () {
+    $('.features__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+
+    });
+
+});
+
