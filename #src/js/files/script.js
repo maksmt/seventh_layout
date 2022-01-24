26796d511cb411c3ea75a6e7f7f8d0254a54b1da
maksmt <maksmtoff@gmail.com>
2022-01-24 09:33:14 +0200

    first

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -30,3 +30,18 @@ $(document).ready(function () {
 
 });
 
+var scrolled;
+window.onscroll = function () {
+    scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrolled > 20) {
+        $(".header").css({
+            "box-shadow": "0 5px 5px -5px #b66161",
+        })
+    }
+    if (scrolled < 20) {
+        $(".header").css({
+            "box-shadow": "0 0px 0px 0px #b66161",
+        })
+    }
+}
